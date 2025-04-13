61a6d0169fa9289f6d79d46f3ca71d9c26692074
AndrewBalin <i@andrew-balin.ru>
2025-04-13 14:52:46 +0500

    'Scripts'

---
+++ Assets/Scripts/Interactive/Interactable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.UI;
+
+namespace Interactive
+{
+    [RequireComponent(typeof(Collider))]
+    public class Interactable : MonoBehaviour
+    {
+        void Start()
+        {
+            this.GetComponent<CustomPassVolume>().customPasses[0].enabled = false;
+        }
+        
+        public void OnHover()
+        {
+            this.GetComponent<CustomPassVolume>().customPasses[0].enabled = true;
+        }
+        
+        public void OnUnhover()
+        {
+            this.GetComponent<CustomPassVolume>().customPasses[0].enabled = false;
+        }
+        
+        public void PerformAction(string action)
+        {
+            Debug.Log($"Action {action} performed on {gameObject.name}");
+            
+            // Логика взаимодействия
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/IsoCameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Movement
+{
+    public class IsoCameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        public Vector3 offset = new Vector3(-10f, -8f, -10f);
+        public float smoothSpeed = 7f;
+
+        void LateUpdate()
+        {
+            if (target == null) return;
+
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+
+            transform.LookAt(target);
+        }
+    }
+}
